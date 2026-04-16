415883ac5fa7d81fcec95c07a5c73cf752d794b6
fserakib26 <fserakib26@gmail.com>
2026-04-17 03:32:42 +0600

    React Structure Created

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+import Friends from "../pages/friends/Friends";
+import Homepage from "../pages/homepage/Homepage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout/>,
+    children: [
+      {
+        index: true,
+        element: <Homepage/>,
+      },      
+      {
+        path: "/friends",
+        element: <Friends/>,
+      }
+    ],
+  },
+  
+]);
