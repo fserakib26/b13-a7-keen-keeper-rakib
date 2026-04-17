2cd63f0da8b8041728212eca5c8fa428f2781346
fserakib26 <fserakib26@gmail.com>
2026-04-17 15:05:49 +0600

    Friends Details page completed

--- src/pages/friendDetails/FriendDetails.jsx
+++ src/pages/friendDetails/FriendDetails.jsx
@@ -1,5 +1,7 @@
 import React, { use } from 'react'
-import { FaArchive } from 'react-icons/fa'
+import { FaArchive, FaVideo } from 'react-icons/fa'
+import { FiPhoneCall } from 'react-icons/fi'
+import { IoMdText } from 'react-icons/io'
 import { MdDeleteForever, MdOutlineSnooze } from 'react-icons/md'
 import { useLoaderData, useParams } from 'react-router'
 
@@ -52,29 +54,54 @@ const FriendDetails = () => {
         </div>
         <div className='w-full p-5'>
            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-evenly items-center gap-4'>
-                <div className='text-center border border-gray-100 shadow-md rounded-2xl w-full mb-5 p-15'>
+                <div className='text-center border border-gray-100 shadow-md rounded-2xl w-full mb-5 p-13'>
                     <h2 className='font-bold text-3xl text-[#244d3f]'>{expectedFriend.days_since_contact}</h2>
                     <p>Days since Contact</p>
                 </div>
-                <div className='text-center border border-gray-100 shadow-md rounded-2xl w-full mb-5 p-15'>
-                    <h2 className='font-bold text-3xl text-[#244d3f]'>{expectedFriend.days_since_contact}</h2>
-                    <p>Days since Contact</p>
+                <div className='text-center border border-gray-100 shadow-md rounded-2xl w-full mb-5 p-13'>
+                    <h2 className='font-bold text-3xl text-[#244d3f]'>{expectedFriend.goal}</h2>
+                    <p>Goal</p>
                 </div>
-                <div className='text-center border border-gray-100 shadow-md rounded-2xl w-full mb-5 p-15'>
-                    <h2 className='font-bold text-3xl text-[#244d3f]'>{expectedFriend.days_since_contact}</h2>
-                    <p>Days since Contact</p>
+                <div className='text-center border border-gray-100 shadow-md rounded-2xl w-full mb-5 p-13'>
+                    <h2 className='font-bold text-3xl text-[#244d3f]'>{expectedFriend.next_due_date}</h2>
+                    <p>Next Due</p>
                 </div>
                 
             </div> 
 
             <div className='grid grid-cols-1 justify-evenly items-center gap-4'>
-                <div className='text-center border border-gray-100 shadow-md rounded-2xl w-full mb-5 p-15'>
-                    
+                <div className='border border-gray-100 shadow-md rounded-2xl w-full mb-5 px-15 py-13'>
+                    <div className='flex justify-between'>
+                        <div>
+                            <h2>Relationship Goal</h2>
+                        </div>
+                        <div>
+                            <button className='btn'>Edit</button>
+                        </div>
+                    </div>
+                    <div>
+                        <p>Connect every <span className='font-bold'>30 days</span></p>
+                    </div>
                 </div>
             </div>
             <div className='grid grid-cols-1 justify-evenly items-center gap-4'>
-                <div className='text-center border border-gray-100 shadow-md rounded-2xl w-full mb-5 p-15'>
-                    
+                <div className='border border-gray-100 shadow-md rounded-2xl w-full mb-5 px-15 py-5'>
+                    <h2 className='font-bold pb-3'>Quick Check-In</h2>
+                    <div className='flex justify-between gap-4'>
+                        <div className='bg-[#e9fbf3] border-2 shadow-2xs border-[#e9e9e9] p-5 w-full text-center rounded-2xl'>
+                            <p className='text-2xl flex items-center justify-center '><FiPhoneCall /></p> <br />
+                            <p>Call</p>
+                        </div>
+                        <div className='bg-[#e9fbf3] border-2 shadow-2xs border-[#e9e9e9] p-5 w-full text-center rounded-2xl'>
+                            <p className='text-2xl flex items-center justify-center '><IoMdText /></p> <br />
+                            <p>Text</p>
+                        </div>
+                        <div className='bg-[#e9fbf3] border-2 shadow-2xs border-[#e9e9e9] p-5 w-full text-center rounded-2xl'>
+                            <p className='text-2xl flex items-center justify-center'><FaVideo /></p> <br />
+                            <p>Video</p>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
